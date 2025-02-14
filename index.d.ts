3f0a43611fb03c913072b6f78c2597ae9309a875
WillyRamirez <WillyRamirez@users.noreply.github.com>
2025-02-14 12:13:42 +0100

    Create index.d.ts
    added declaration file

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+declare module "react-native-smooth-pincode-input" {
+  import React from 'react';
+  import { ViewStyle, TextStyle } from 'react-native';
+
+  export interface SmoothPinCodeInputProps {
+    codeLength?: number;
+    value: string;
+    onTextChange: (code: string) => void;
+    onFulfill?: (code: string) => void;
+    cellStyle?: ViewStyle;
+    cellStyleFocused?: ViewStyle;
+    cellTextStyle?: TextStyle;
+    // ... add any other props that your fork supports
+  }
+
+  const SmoothPinCodeInput: React.FC<SmoothPinCodeInputProps>;
+
+  export default SmoothPinCodeInput;
+}
